1f679e52b3900c6685b6fd99ea6473e45178609c
LizVel <Elizaveligurova@gmail.com>
2021-12-22 22:33:29 +0300

    Traversal

---
+++ Traversal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3dfg
+{
+    public class Travers
+    {
+        public static void Traversal(derevo node) // Обратный обход
+        {
+            var stack = new Stack<derevo>(); // создание стэка для обхода
+            derevo lastNodeVisited = null;
+
+            while (stack.Count > 0 || node != null)
+            {
+                if (node != null)
+                {
+                    stack.Push(node); // Сначала обходим левое поддерево
+                    node = node.Left;
+                }
+                else
+                {
+                    var peekNode = stack.Peek();
+                    if (peekNode.Right != null && lastNodeVisited != peekNode.Right)
+                    {
+                        node = peekNode.Right; // обходим правое поддерево
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                        Console.Write(string.Join(Environment.NewLine, peekNode.Infa));
+                        lastNodeVisited = stack.Pop(); // Достаём из стэка и выводим
+                    }
+                }
+            }
+        }
+    }
+}
