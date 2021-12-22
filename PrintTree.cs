4eb6818230e4f029fe25082a63bbdb0351959bff
kaaatle <bruzgina.ek@gmail.com>
2021-12-22 21:02:53 +0300

    PrintTree

---
+++ PrintTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3dfg
+{
+    public class countDerevo
+    {
+        public static void Print(derevo node)
+        {
+            if (node != null) // изначальная проверка не пусто ли в дереве
+            {
+                if (node.koren == null) // для первого вывода 
+                {
+                    Console.WriteLine("Корень дерева:{0}", node.Infa);
+                }
+                else
+                {
+                    if (node.koren.Left == node)
+                    {
+                        Console.WriteLine("Слева от {1}  - {0}", node.Infa, node.koren.Infa);
+                    }
+
+                    if (node.koren.Right == node)
+                    {
+                        Console.WriteLine("Справа от {1} - {0}", node.Infa, node.koren.Infa);
+                    }
+                }
+                if (node.Left != null)
+                {
+                    Print(node.Left); // применяем рекурсию для того, чтобы программа не заканчивалась после вывода корня и вывелись все значения
+                }
+                if (node.Right != null)
+                {
+                    Print(node.Right); // Проверяем правое дерево и применяем рекурсию
+                }
+            }
+        }
+    }
+}
